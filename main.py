476a3abcbaf4ee8a68509e45b5b0f3c95f7d38ee
DrunkCookies0 <atollhouse2@gmail.com>
2024-09-13 16:38:33 -0400

    Update main.py

--- main.py
+++ main.py
@@ -20,14 +20,14 @@ async def checktier(
         description="Choose the format (1v1, 2v2, 3v3)",
         choices=["1v1", "2v2", "3v3"]
     ),
-    peak3s: int = SlashOption(
+    peak3s: str = SlashOption(
         name="peak3s",
         description="Peak MMR for 3v3",
         default=0,
         min_value=0,
         max_value=2500
     ),
-    peak2s: int = SlashOption(
+    peak2s: str = SlashOption(
         name="peak2s",
         description="Peak MMR for 2v2",
         default=0,
